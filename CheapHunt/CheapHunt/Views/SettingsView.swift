301f04f895c8e891cb2509a6a809f8867cf3cd88
Furkan Hanci <furkanhanci265@gmail.com>
2021-06-04 11:36:01 +0300

    SettingsView has been added

---
+++ CheapHunt/CheapHunt/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  CheapHunt
+//
+//  Created by Furkan Hanci on 6/4/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
