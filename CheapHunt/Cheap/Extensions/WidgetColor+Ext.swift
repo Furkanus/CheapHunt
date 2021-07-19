a7c8314e002d41f1969993a8d67458ac4ebd53e1
Furkan Hanci <furkanhanci265@gmail.com>
2021-07-19 16:45:23 +0300

    Readme update

---
+++ CheapHunt/Cheap/Extensions/WidgetColor+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  WidgetColor+Ext.swift
+//  CheapExtension
+//
+//  Created by Furkan Hanci on 7/19/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let bg1 = Color(red: 151 / 250, green: 217 / 250, blue: 225 / 250)
+    static let bg2 = Color(red: 217 / 250, green: 175 / 250, blue: 117 / 250)
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CheapHunt
 Cheap Hunt is an iOS App written with SwiftUI.You can fetch games for your amount.
 
+Also added big update Widget Feature 🚀
 ![Apple iPhone 11 Presentation](https://user-images.githubusercontent.com/60580341/123104051-44f70680-d43f-11eb-95b5-0546816733f9.png)
 
